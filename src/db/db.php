04b2f16fddc85a5f8b4182fad71175e09fabec1b
Lucas Ribeiro <lucas.develop@outlook.com.br>
2026-03-29 15:08:17 -0300

    add: PDO database connection

---
+++ src/db/db.php
@@ -0,0 +1,18 @@
+<?php 
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false
+];
+
+$pdo = null;
+
+try{
+    $pdo = new PDO($dsn, $dbUser, $dbPassword, $options);
+} catch (\PDOException $e){
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+?>
