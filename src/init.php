06f6b9c221df7354627327fa0a431f1fb5d5601b
Lucas Ribeiro <lucas.develop@outlook.com.br>
2026-03-29 09:40:11 -0300

    add: init bootstrap file - session, config and helpers

---
+++ src/init.php
@@ -0,0 +1,10 @@
+<?php 
+
+session_start();
+
+require_once __DIR__ . "/../config/config.php";
+require_once __DIR__ . "/db/db.php";
+require_once __DIR__ . "/helpers/csrf.php";
+require_once __DIR__ . "/helpers/validate.php";
+
+?>
